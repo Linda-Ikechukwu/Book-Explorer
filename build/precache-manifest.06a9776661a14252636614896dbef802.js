69424dec508d165a595a514dc49405a0fe744789
Linda Ikechukwu <lindaedwin96@gmail.com>
2019-01-13 20:20:13 +0100

    initial commit

---
+++ build/precache-manifest.06a9776661a14252636614896dbef802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2a4c888dd2e5ae707a2c",
+    "url": "/static/js/main.2a4c888d.chunk.js"
+  },
+  {
+    "revision": "6bd7c48c5064ce66b1a1",
+    "url": "/static/js/1.6bd7c48c.chunk.js"
+  },
+  {
+    "revision": "2a4c888dd2e5ae707a2c",
+    "url": "/static/css/main.92513018.chunk.css"
+  },
+  {
+    "revision": "5f61c9d4e60d89a63b9f54bf8aaa3a3a",
+    "url": "/index.html"
+  }
+];
